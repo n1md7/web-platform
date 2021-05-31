6b2a78792e72d9ee3247657b5fc38190d560daa9
n1md7 <b.kodua@gmail.com>
2021-05-31 21:55:41 +0300

    #22 node mailer added

---
+++ server/src/services/MailService.ts
@@ -0,0 +1,34 @@
+import nodemailer from "nodemailer";
+import mailTransporter from "../config/mail";
+import logWrite from '../logger';
+
+
+export type SendType = {
+  from: string;
+  to: string;
+  subject: string;
+  text: string;
+  html: string;
+}
+export default class MailService {
+
+  static async send(props: SendType): Promise<void> {
+    const transporter = await mailTransporter;
+    const mail = await transporter.sendMail({
+      from: '"Fred Foo 👻" <foo@example.com>', // sender address
+      to: "b.kodua@gmail.com", // list of receivers
+      subject: "Hello ✔", // Subject line
+      text: "Hello world?", // plain text body
+      html: "<b>Hello world?</b>", // html body
+      ...props
+    });
+
+    logWrite.info(`Message sent: ${mail.messageId}`);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+    // Preview only available when sending through an Ethereal account
+    console.log(`Preview URL: ${nodemailer.getTestMessageUrl(mail)}`);
+    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+  }
+
+}
